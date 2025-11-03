f432e11dc7a85d942841ff4432fcdc5f09160b60
Lukáš Zdražílek <147374351+LukasZdrazilek@users.noreply.github.com>
2025-11-03 17:28:03 +0100

    Pridani ukolu 4.1

---
+++ Ukol4.1_numlock.ps1
@@ -0,0 +1,7 @@
+# Zjištění aktuální hodnoty
+$currentValue = Get-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name InitialKeyboardIndicators
+
+# Pokud NumLock není zapnutý, nastavíme hodnotu na 2
+if ($currentValue.InitialKeyboardIndicators -ne '2') {
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name InitialKeyboardIndicators -Value '2'
+}
